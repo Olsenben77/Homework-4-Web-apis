c5b0cff07fd44061e03abd3239927f0769345bef
Ben Olsen <olsenben1992@gmail.com>
2019-12-13 11:49:23 -0800

    Added array questions

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Arrays are accesed using ______________ notation:",
+      choices: ["Bracket", "Curly Brackets", "Parentheses", "Array"],
+      answer: "Bracket"
+    },
+    {
+      title: "There are/is _________ numeric type(s) in JavaScript.",
+      choices: ["One", "Two", "Three", "Infinite"],
+      answer: "One"
+    },
+    {
+    title: "Which function determines whether a number is an illegal value?",
+    choices: ["init", "numberSelect", "isNaN", "valueSelect"],
+    answer: "isNaN"
+     },
+     {
+  title: "What symbol means to increment?",
+  choices: ["+", "===", "**", "++"],
+  answer: "++"
+    },
+    {
+title: "Which type of quotation marks do we use in Strings?",
+choices: ["double quotes", "single quotes", "both work, just be consistent", "neither"],
+answer: "parentheses"
+    },
+    ];
+  
