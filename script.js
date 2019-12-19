00782d6c25913187a4e35ccada65be583eb8c12f
Ben Olsen <olsenben1992@gmail.com>
2019-12-18 17:01:18 -0800

    HTML and CSS changes

--- script.js
+++ script.js
@@ -1,44 +1,50 @@
-
-startQuiz.addEventListener('click', )
-
+startQuiz.addEventListener("click");
 
 var questions = [
-    {
-      title: "Arrays are accesed using ______________ notation:",
-      choices: ["Bracket", "Curly Brackets", "Parentheses", "Array"],
-      answer: "Bracket"
-    },
-    {
-      title: "There are/is _________ numeric type(s) in JavaScript.",
-      choices: ["One", "Two", "Three", "Infinite"],
-      answer: "One"
-    },
-    {
+  {
+    title: "Arrays are accesed using ______________ notation:",
+    choices: ["Bracket", "Curly Brackets", "Parentheses", "Array"],
+    answer: "Bracket"
+  },
+  {
+    title: "There are/is _________ numeric type(s) in JavaScript.",
+    choices: ["One", "Two", "Three", "Infinite"],
+    answer: "One"
+  },
+  {
     title: "Which function determines whether a number is an illegal value?",
     choices: ["init", "numberSelect", "isNaN", "valueSelect"],
     answer: "isNaN"
-     },
-     {
-  title: "What symbol means to increment?",
-  choices: ["+", "===", "**", "++"],
-  answer: "++"
-    },
-    {
-title: "Which type of quotation marks do we use in Strings?",
-choices: ["double quotes", "single quotes", "both work, just be consistent", "neither"],
-answer: "parentheses"
-    },
-    {
+  },
+  {
+    title: "What symbol means to increment?",
+    choices: ["+", "===", "**", "++"],
+    answer: "++"
+  },
+  {
+    title: "Which type of quotation marks do we use in Strings?",
+    choices: [
+      "double quotes",
+      "single quotes",
+      "both work, just be consistent",
+      "neither"
+    ],
+    answer: "parentheses"
+  },
+  {
     title: "What cereal was Spencer eating in class last Saturday?",
-choices: ["Lucky Charms", "Cinnamon Toast Crunch", "Cheerios", "Count Chocula"],
-answer: "Cheerios"
-    },
-    ];
+    choices: [
+      "Lucky Charms",
+      "Cinnamon Toast Crunch",
+      "Cheerios",
+      "Count Chocula"
+    ],
+    answer: "Cheerios"
+  }
+];
 
+var score = 0;
 
-
-var score = 0
-
-for (var i=0; i < questions.length; i++){
-    var response
+for (var i = 0; i < questions.length; i++) {
+  var response;
 }
